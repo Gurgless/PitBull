6c8f4189ef0a386b644303351b9a7d468023aee5
Gurgless <107460037+Gurgless@users.noreply.github.com>
2022-09-05 19:44:03 +0700

    update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -2,7 +2,7 @@ let handler = m => m
 
 let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
-handler.before = function (m, { isOwner, isAdmin, isBotAdmin }) {
+handler.before = function (m, { isOwner, isAdmin, isBotAdmin}) {
     if (m.isBaileys && m.fromMe && isOwner) return !0
     let chat = db.data.chats[m.chat]
     let user = db.data.users[m.sender]
@@ -20,7 +20,7 @@ ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
 “Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', 'ASTAGHFIRULLAH', '.maaf', m)*/
         /*if (user.warning >= 10) {
             user.banned = true*/
-                  conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
+                  this.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
                 	/*this.groupParticipantsUpdate(m.chat, [m.sender], "remove")*/
                    //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
     }
