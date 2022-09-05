21083d231feac529edfd5fb258951e5d0139269d
Gurgless <107460037+Gurgless@users.noreply.github.com>
2022-09-05 23:17:59 +0700

    update stick-ok.js

--- plugins/stick-ok.js
+++ plugins/stick-ok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, command, text }) => {
  await conn.sendFile(m.chat, 'https://bit.ly/3AUUure', 'haori.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true, contextInfo: {
-        externalAdReply: { showAdAttribution: true, title: `${ucapan()}`, body: `${pickRandom(['Furry Indonesia :3', 'Suka Pokemon Nggak Kak :3', 'Kangen Haori Nggak?', 'Udah makan belum kak?', 'Udah Makan Belum?', 'Semangat ya kak!', 'Jangan begadang mulu ya!', 'Jangan spam ya kak!', 'Jangan lupa donasi yak kak! QωQ', 'Jaga kesehatan yaw kak!', 'Jangan lupa makan!', 'Jangan lupa istirahat yak! UωU', 'Haori Sayang Kamu :3', 'Pr nya udh belum kak?', 'Jangan kebanyakan main hp yk! nanti sakit :‹'])}`, sourceUrl: 'https://www.tiktok.com/@fory_whitecattiktok?_t=8V5TGON5rgv&_r=1', thumbnail: await (await fetch(pickRandom(global.waifu))).buffer(),}} 
+        externalAdReply: { showAdAttribution: true, title: `${ucapan()}`, body: `${pickRandom(['Furry Indonesia :3', 'Kenal Rio Gak Kak ?:3', 'Ownerku GG loh kak :3', 'Suka Rio Nggak Kak :3', 'Kangen Sama Doi Gak ?:3', 'Udah makan belum kak?', 'Udah Makan Belum?', 'Semangat ya kak!', 'Jangan begadang mulu ya!', 'Jangan spam ya kak!', 'Jangan lupa donasi yak kak! QωQ', 'Jaga kesehatan yaw kak!', 'Jangan lupa makan!', 'Jangan lupa istirahat yak! UωU', 'R-BOT Sayang Kamu :3', 'Pr nya udh belum kak?', 'Jangan kebanyakan main hp ya! nanti sakit :‹'])}`, sourceUrl: 'https://instagram/riyoshi_dyxn', thumbnail: await (await fetch(pickRandom(global.waifu))).buffer(),}} 
      })
  } 
  handler.customPrefix = /^(iya|ok|oke|okey)$/i
