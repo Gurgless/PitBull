79d903f4894955c29e62b57554d0c45bc1004c9a
Gurgless <107460037+Gurgless@users.noreply.github.com>
2022-09-05 19:11:49 +0700

    update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -8,7 +8,7 @@ handler.before = function (m, { isOwner, isAdmin, isBotAdmin }) {
     let user = db.data.users[m.sender]
     let isBadword = badwordRegex.exec(m.text)
 
-    if (!chat.antiBadword && !chat.isBanned && !user.banned && isBadword && !isOwner && !isAdmin) {
+    if (m.isGroup && isBotAdmin &&!chat.antiBadword && !chat.isBanned && !user.banned && isBadword && !isOwner && !isAdmin) {
         /*user.warning += 1
         this.send2Button(m.chat, `*Badword terdeteksi!*
 Warning: ${user.warning} / 10
@@ -20,15 +20,9 @@ ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
 “Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', 'ASTAGHFIRULLAH', '.maaf', m)*/
         /*if (user.warning >= 10) {
             user.banned = true*/
-            if (m.isGroup) {
-                if (isBotAdmin) {
                   conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.id, participant: m.sender } })
                 	/*this.groupParticipantsUpdate(m.chat, [m.sender], "remove")*/
                    //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-                }
-            }
-        }
     }
-    return !0
 }
 module.exports = handler
