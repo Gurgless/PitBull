7d83dd0b6beefaa39e4bd106e69c53994b7bd7e1
Gurgless <107460037+Gurgless@users.noreply.github.com>
2022-09-09 07:18:09 +0700

    update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,6 +1,6 @@
 let handler = m => m
 
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asu|anj|babi|pig|asshole/i // tambahin sendiri
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asu|anj|babi|pig|asshole|cum|cikablyat|slut/i // tambahin sendiri
 
 handler.before = function (m, { isOwner, isAdmin, isBotAdmin}) {
     if (m.isBaileys && m.fromMe && isOwner) return !0
